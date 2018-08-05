66e981f1ab51c4481d4eb6d54cb0366950712d4e
Andrey Smirnov <iadisey@gmail.com>
2018-08-05 11:22:42 +0300

    Move DIR init from \source\bus\init to \source\init

--- source/index.js
+++ source/index.js
@@ -5,24 +5,17 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 // Instruments
-import { store } from './bus/init/store';
-import './theme/initGallery';
+import { store } from './init/store';
+// import './theme/initGallery';
 
 // App
 import App from './navigation/App';
 
-render (
-    <Provider store={store}>
+render(
+    <Provider store = { store }>
         <Router>
-            <App/>
+            <App />
         </Router>
     </Provider>,
-    document.getElementById ('app')
+    document.getElementById('app')
 );
-
-
-// ToDo: ДЗ 1 - Добавить в API
-// ToDo: ДЗ 1 - Создать тип Экшена
-// ToDo: ДЗ 1 - Создать Экшен
-// ToDo: ДЗ 1 - Создать обработку редюсером (новый пост в начало смиска)
-// ToDo: ДЗ 1 - Привязать новый экшени через мэпдиспетч то пропс к пропсам компонента (копозер должен получить его автоматически)

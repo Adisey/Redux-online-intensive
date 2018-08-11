8d24945cdbfe47c55c9d32cace92170d8e364736
Andrey Smirnov <iadisey@gmail.com>
2018-08-11 23:52:03 +0300

    Lesson 4 Finish part 8 (8. Продвинутая аутентификация и роутинг — 7 часть) (Удаление старого токина, после неудачной попітки его использовать)

--- source/bus/auth/saga/workers/authenticate.js
+++ source/bus/auth/saga/workers/authenticate.js
@@ -13,10 +13,15 @@ export function* authenticate () {
         const { data: profile, message } = yield apply(response, response.json);
 
         if (response.status !== 200) {
+            if (response.status === 401) {
+                yield apply(localStorage, localStorage.removeItem, ['token']);
+                yield apply(localStorage, localStorage.removeItem, ['remember']);
+
+                return null;
+            }
             throw new Error(message);
         }
         yield apply(localStorage, localStorage.setItem, ['token', profile.token]);
-
         yield put(profileActions.fillProfile(profile));
         yield put(authAction.authenticate());
     } catch (error) {

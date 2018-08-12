a3b337e49f5c8f3a9215326b2173637425d907cb
Andrey Smirnov <iadisey@gmail.com>
2018-08-12 21:19:32 +0300

    Lesson 4 Home Work Complete (Работает удаление поста)

--- source/bus/posts/reducer.js
+++ source/bus/posts/reducer.js
@@ -23,20 +23,37 @@ export const postsReducer = (state = initalState, action) => {
         case type.CLEAR_POSTS:
             return state.clear();
         case type.REMOVE_POST:
-            console.log(`state ->`, state);
-            console.log(`action.payload ->`, action.payload);
-            state.map((post) =>{
-                // debugger;
+            // console.log(`state ->`, state);
+            // console.log(`action.payload ->`, action.payload);
+            // state.map((post) => {
+            //     console.log(`post ->`, post);
+            //     post.map((el, ne) => {
+            //         const _yes = ne === 'id'?'_!_': '___';
+            //         console.log(`${ne} - ${_yes} ->`, el);
+            //     });
+            // });
+            const newState = state.filter((post) => {
+                let isDeleteID = false;
+
                 console.log(`post ->`, post);
-                post.map((el)=>{
-                    console.log(`el ->`, el);
+
+                post.map((el, ne) => {
+                    console.log(`${ne} ->`, el);
+                    if (ne === 'id' && el === action.payload) {
+                        console.log(`!!!!!!!!!! ----------------->`);
+                        isDeleteID = true;
+                    }
                 });
+                console.log(`isDeleteID ->`, isDeleteID);
+
+                return !isDeleteID;
             });
-            // const newState = state.filter(({post})=> post.id !== action.payload);
-            // console.log(`newState ->`, newState);
-            return state;
-            //return state.unshift(fromJS(action.payload));
-            // ToDo: Реализовать удаление поста
+            // const newState = state.filter((post)=> post.id !== action.payload);
+
+            console.log(`newState ->`, newState);
+
+            return newState;
+        // ToDo: Реализовать удаление поста
 
         default:
             return state;

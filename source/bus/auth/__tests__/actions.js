e1c0c3d2ca4d4ff8d5c8cddb79ba9cb9727c8189
Andrey Smirnov <iadisey@gmail.com>
2018-08-27 20:19:24 +0300

    Lesson 9 - Тестирование программного кода. Jest 2. Введение в тестирование Redux — 1 часть Action Auth
    Lesson 8 Home Work not Complete (Update password not work)

---
+++ source/bus/auth/__tests__/actions.js
@@ -0,0 +1,68 @@
+/**
+ * Created by PhpStorm
+ * Project p501-redux-online-intensive
+ * User: Adisey
+ * Date: 27.08.2018
+ * Time: 19:39
+ */
+
+// Actions
+import { authAction } from '../actions';
+
+// Types
+import { type } from '../types';
+
+describe('Экшены аутентификации:', () => {
+    test('authenticate', () => {
+        expect(authAction.authenticate()).toEqual({
+            type: type.AUTHENTICATE,
+        });
+    });
+
+    test('initialialze', () => {
+        expect(authAction.initialialze()).toEqual({
+            type: type.INITIALIZE,
+        });
+    });
+
+    test('logout', () => {
+        expect(authAction.logout()).toEqual({
+            type: type.LOGOUT,
+        });
+    });
+
+    test('signupAsync', () => {
+        expect(authAction.signupAsync(__.userProfile)).toEqual({
+            type:    type.SIGNUP_ASYNC,
+            payload: __.userProfile,
+        });
+    });
+
+    test('loginAsync', () => {
+        expect(authAction.loginAsync(__.credentials)).toEqual({
+            type:    type.LOGIN_ASYNC,
+            payload: __.credentials,
+        });
+    });
+
+    test('authenticateAsync', () => {
+        expect(authAction.authenticateAsync()).toEqual({
+            type: type.AUTHENTICATE_ASYNC,
+        });
+    });
+
+    test('initializedAsync', () => {
+        expect(authAction.initializedAsync()).toEqual({
+            type: type.INITIALIZE_ASYNC,
+        });
+    });
+
+    test('logoutAsync', () => {
+        expect(authAction.logoutAsync()).toEqual({
+            type: type.LOGOUT_ASYNC,
+        });
+    });
+
+
+
+});

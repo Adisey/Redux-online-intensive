9599ecaaf926ac67c829e75f7b038ebe3316abb7
Andrey Smirnov <iadisey@gmail.com>
2018-09-03 16:11:18 +0300

    Lesson 10 - Продвинутое тестирование Redux (Test Profile reducer with Snapschot)

--- source/bus/profile/__tests__/reducer.js
+++ source/bus/profile/__tests__/reducer.js
@@ -6,39 +6,84 @@
  * Time: 17:51
  */
 // Core
-import { Map } from 'immutable';
+import { Map } from "immutable";
 // Reducer
-import { profileReducer } from '../reducer';
+import { profileReducer } from "../reducer";
 // Actions
-import { profileActions } from '../actions';
+import { profileActions } from "../actions";
 
 const initalState = Map({
-    id:        '',
-    firstName: '',
-    lastName:  '',
-    avatar:    '',
-    token:     '',
+    id:        "",
+    firstName: "",
+    lastName:  "",
+    avatar:    "",
+    token:     "",
 });
 
-describe('Редюсер Profile:', () => {
-    test('Возврат состояния по умолчанию', () => {
-        expect(profileReducer(void 0, {}))
-            .toEqual(initalState);
+describe("Редюсер Profile:", () => {
+    test("Возврат состояния по умолчанию", () => {
+        expect(profileReducer(void 0, {})).toEqual(initalState);
     });
 
-    test('Возврат состояния FILL_PROFILE', () => {
+    test("Возврат состояния по умолчанию Snapshot", () => {
+        expect(profileReducer(void 0, {})).toMatchInlineSnapshot(`
+Immutable.Map {
+  "id": "",
+  "firstName": "",
+  "lastName": "",
+  "avatar": "",
+  "token": "",
+}
+`);
+    });
+
+    test("Возврат состояния FILL_PROFILE", () => {
+        expect(
+            profileReducer(void 0, profileActions.fillProfile(__.userProfile))
+        ).toEqual(Map(__.userProfile));
+    });
+
+    test("Возврат состояния FILL_PROFILE Snapshot", () => {
         expect(profileReducer(void 0, profileActions.fillProfile(__.userProfile)))
-            .toEqual(Map(__.userProfile));
+            .toMatchInlineSnapshot(`
+Immutable.Map {
+  "id": "TEST_ID",
+  "firstName": "Walter",
+  "lastName": "White",
+  "avatar": "TEST_AVATAR",
+  "token": "TEST_TOKEN",
+}
+`);
     });
 
-    test('Возврат состояния UPDATE_AVATAR', () => {
-        expect(profileReducer(void 0, profileActions.updateAvatar(__.newAvatarUrl)))
-            .toEqual(Map(initalState).set('avatar', __.newAvatarUrl));
+    test("Возврат состояния UPDATE_AVATAR Snapshot", () => {
+        expect(profileReducer(void 0, profileActions.updateAvatar(__.url)))
+            .toMatchInlineSnapshot(`
+Immutable.Map {
+  "id": "",
+  "firstName": "",
+  "lastName": "",
+  "avatar": "https://www.url.com",
+  "token": "",
+}
+`);
     });
 
-    test('Возврат состояния CLEAR_PROFILE', () => {
-        expect(profileReducer(void 0, profileActions.clearProfile(__.userProfile)))
-            .toEqual(Map({}));
+    test("Возврат состояния UPDATE_AVATAR", () => {
+        expect(
+            profileReducer(void 0, profileActions.updateAvatar(__.newAvatarUrl))
+        ).toEqual(Map(initalState).set("avatar", __.newAvatarUrl));
     });
 
+    test("Возврат состояния CLEAR_PROFILE Snapshot", () => {
+        expect(
+            profileReducer(void 0, profileActions.clearProfile())
+        ).toMatchInlineSnapshot(`Immutable.Map {}`);
+    });
+
+    test("Возврат состояния CLEAR_PROFILE", () => {
+        expect(
+            profileReducer(void 0, profileActions.clearProfile(__.userProfile))
+        ).toEqual(Map({}));
+    });
 });

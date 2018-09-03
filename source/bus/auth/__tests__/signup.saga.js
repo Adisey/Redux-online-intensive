25f27b42bd08f178aa9553edd85ddf6ef77c1aff
Andrey Smirnov <iadisey@gmail.com>
2018-09-03 23:34:07 +0300

    Lesson 11 - Продвинутое тестирование Redux Saga (3 - проверка неуспешного ответа сервера - 400)

--- source/bus/auth/__tests__/signup.saga.js
+++ source/bus/auth/__tests__/signup.saga.js
@@ -15,8 +15,39 @@ import { uiActions } from '../../ui/actions';
 import { signup } from '../saga/workers';
 
 const signupAction = authAction.signupAsync(__.userProfile);
-const saga = cloneableGenerator(signup)();
+const saga = cloneableGenerator(signup)(signupAction);
 let clone = null;
 
+describe(' Signup Saga Worker', () => {
+    describe('До запроса к серверу', () => {
+        test(`Должен запуститься экшен "StartFetching"`, () => {
+            expect(saga.next().value).toEqual(put(uiActions.startFetching()));
+        });
 
+        test(`Должен запуститься fetch запрос"`, () => {
+            expect(saga.next().value).toEqual(apply(api, api.auth.signup, [__.userProfile]));
+            clone = saga.clone();
+        });
+    });
 
+    describe('Ответ сервера статусом 400', () => {
+        test(`Fetch запрос вернул статус 400`, () => {
+            expect(clone.next(__.fetchResponseFail400).value)
+                .toEqual(apply(__.fetchResponseFail400, __.fetchResponseFail400.json));
+        });
+
+        test(`Получение ответа с объектом ошибки `, () => {
+            expect(clone.next(__.responseDataFail).value)
+                .toEqual(put(uiActions.emitError(__.error, 'signup fetchUsers')));
+        });
+
+        test(`Должен запуститься экшен "StopFetching"`, () => {
+            expect(clone.next().value).toEqual(put(uiActions.stopFetching()));
+        });
+
+        test(`Последний шаг генератора`, () => {
+            expect(clone.next().done).toBe(true)
+        });
+
+    });
+});

52a17020b4f5e148820d27ed4a84d465fb0c36b6
Andrey Smirnov <iadisey@gmail.com>
2018-09-03 23:59:05 +0300

    Lesson 11 - Продвинутое тестирование Redux Saga (4 - проверка всех "yield" после успешного ответа сервера - 200)

--- source/bus/auth/__tests__/signup.saga.js
+++ source/bus/auth/__tests__/signup.saga.js
@@ -6,39 +6,43 @@
  * Time: 21:02
  */
 // Core
-import { put, apply } from 'redux-saga/effects';
-import { cloneableGenerator } from 'redux-saga/utils';
+import { put, apply } from "redux-saga/effects";
+import { cloneableGenerator } from "redux-saga/utils";
 
-import { api } from '../../../REST';
-import { authAction } from '../../auth/actions';
-import { uiActions } from '../../ui/actions';
-import { signup } from '../saga/workers';
+import { api } from "../../../REST";
+import { authAction } from "../../auth/actions";
+import { uiActions } from "../../ui/actions";
+import { signup } from "../saga/workers";
 
 const signupAction = authAction.signupAsync(__.userProfile);
 const saga = cloneableGenerator(signup)(signupAction);
 let clone = null;
 
-describe(' Signup Saga Worker', () => {
-    describe('До запроса к серверу', () => {
+describe(" Signup Saga Worker", () => {
+    describe("До запроса к серверу", () => {
         test(`Должен запуститься экшен "StartFetching"`, () => {
             expect(saga.next().value).toEqual(put(uiActions.startFetching()));
         });
 
         test(`Должен запуститься fetch запрос"`, () => {
-            expect(saga.next().value).toEqual(apply(api, api.auth.signup, [__.userProfile]));
+            expect(saga.next().value).toEqual(
+                apply(api, api.auth.signup, [__.userProfile])
+            );
             clone = saga.clone();
         });
     });
 
-    describe('Ответ сервера статусом 400', () => {
+    describe("Ответ сервера статусом 400", () => {
         test(`Fetch запрос вернул статус 400`, () => {
-            expect(clone.next(__.fetchResponseFail400).value)
-                .toEqual(apply(__.fetchResponseFail400, __.fetchResponseFail400.json));
+            expect(clone.next(__.fetchResponseFail400).value).toEqual(
+                apply(__.fetchResponseFail400, __.fetchResponseFail400.json)
+            );
         });
 
         test(`Получение ответа с объектом ошибки `, () => {
-            expect(clone.next(__.responseDataFail).value)
-                .toEqual(put(uiActions.emitError(__.error, 'signup fetchUsers')));
+            expect(clone.next(__.responseDataFail).value).toEqual(
+                put(uiActions.emitError(__.error, "signup fetchUsers"))
+            );
         });
 
         test(`Должен запуститься экшен "StopFetching"`, () => {
@@ -46,7 +50,58 @@ describe(' Signup Saga Worker', () => {
         });
 
         test(`Последний шаг генератора`, () => {
-            expect(clone.next().done).toBe(true)
+            expect(clone.next().done).toBe(true);
+        });
+    });
+
+    describe("Ответ сервера успешным статусом 200", () => {
+        test(`Fetch запрос вернул статус 200`, () => {
+            expect(saga.next(__.fetchResponseSuccess).value).toEqual(
+                apply(__.fetchResponseSuccess, __.fetchResponseSuccess.json)
+            );
+        });
+
+        test(`Проверяем єкшен "fillProfile"`, () => {
+            expect(saga.next(__.responseDataSuccess).value).toMatchInlineSnapshot(`
+Object {
+  "@@redux-saga/IO": true,
+  "PUT": Object {
+    "action": Object {
+      "payload": Object {
+        "avatar": "TEST_AVATAR",
+        "firstName": "Walter",
+        "id": "TEST_ID",
+        "lastName": "White",
+        "token": "TEST_TOKEN",
+      },
+      "type": "FILL_PROFILE",
+    },
+    "channel": null,
+  },
+}
+`);
+        });
+
+        test(`Проверяем єкшен "authenticate"`, () => {
+            expect(saga.next().value).toMatchInlineSnapshot(`
+Object {
+  "@@redux-saga/IO": true,
+  "PUT": Object {
+    "action": Object {
+      "type": "AUTHENTICATE",
+    },
+    "channel": null,
+  },
+}
+`);
+        });
+
+        test(`Должен запуститься экшен "StopFetching"`, () => {
+            expect(saga.next().value).toEqual(put(uiActions.stopFetching()));
+        });
+
+        test(`Последний шаг генератора`, () => {
+            expect(saga.next().done).toBe(true);
         });
 
     });
